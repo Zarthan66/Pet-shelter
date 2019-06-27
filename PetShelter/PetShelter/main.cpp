f09ee1469a29596025a35780aab2f119f083ffae
Zarthan <46505448+Zarthan66@users.noreply.github.com>
2019-06-28 02:36:02 +0700

    Update main.cpp

--- PetShelter/PetShelter/main.cpp
+++ PetShelter/PetShelter/main.cpp
@@ -7,10 +7,6 @@ int main()
 {
 	// To avoid the object get the same emotion
 	srand(time(NULL));
-
-	//Dogs dog1("Siberian Husky");
-	//dog1.PrintIdentity();
-	//dog1.Speak();
 	
 	mainProgram a;
 	a.menu();
