2a7b84d29f2efe14d972785cf79048556eb366ca
LLENN <46107821+LLENN8@users.noreply.github.com>
2019-06-27 21:16:43 +0700

    Update main.cpp

--- PetShelter/PetShelter/main.cpp
+++ PetShelter/PetShelter/main.cpp
@@ -8,7 +8,10 @@ int main()
 	// To avoid the object get the same emotion
 	srand(time(NULL));
 
-	Dogs dog1("Siberian Husky");
-	dog1.PrintIdentity();
-	dog1.Speak();
+	//Dogs dog1("Siberian Husky");
+	//dog1.PrintIdentity();
+	//dog1.Speak();
+	
+	mainProgram a;
+	a.menu();
 }
